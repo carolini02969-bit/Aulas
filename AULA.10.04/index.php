78b9113508c85d0988e554c9b8ac1e88cab0d157
Carolini Sigal <carolini.02969@aluno.iffar.edu.br>
2026-04-10 09:26:27 -0300

    Create index.php
    SUBINDO O INDEX

---
+++ AULA.10.04/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html> 
+<head> 
+<title> Upload Simples </title> 
+<head> 
+<body> 
+<h1> Enviar Arquivo </h1>
+<form action = "upload.php" method = "POST" enctype = "multipart/form-data">
+    <input type = "file" name = "arquivo"> <br>
+<input type="submit">Enviar</button>
+</form> 
+</body> 
+</html>
